4b37f05be17d4093791f04e54057120bb09f5202
Ninh Dang <Ninhduy2000@gmail.com>
2023-10-26 01:00:38 +0700

    Iloveyou

--- src/main/java/ra/controller/UserController.java
+++ src/main/java/ra/controller/UserController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +29,8 @@ import ra.payload.reponse.MessageReponse;
 import ra.payload.request.LoginRequest;
 import ra.payload.request.SignupRequest;
 import ra.security.CustomUserDetails;
+
+import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin("*")
 @Controller
@@ -50,24 +53,87 @@ public class UserController {
         return "register";
     }
 
+//    @PostMapping("/signup")
+//    public String registerUser(@RequestBody User user) {
+////         if (userService.existsByUserName(signupRequest.getUserName())) {
+////             return ResponseEntity.badRequest().body(new MessageReponse("Error:Username is already"));
+////         }
+////         if (userService.existsByEmail(signupRequest.getEmail())) {
+////             return ResponseEntity.badRequest().body(new MessageReponse("Error:Mail is already"));
+////         }
+//
+//            if (userService.existsByUserName(user.getUserName())) {
+//        // Return a message or redirect with an error message
+//        return "redirect:/signup?error=username";
+//    }
+//
+//    if (userService.existsByEmail(user.getEmail())) {
+//        // Return a message or redirect with an error message
+//        return "redirect:/signup?error=email";
+//    }
+//        User user1 = new User();
+//        user1.setUserName(user.getUserName());
+//        user1.setPassword(encoder.encode(user.getPassword()));
+//        user1.setEmail(user.getEmail());
+//        user1.setPhone(user.getPhone());
+//        user1.setUserStatus(true);
+//
+//        //created ngày
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//        Date dateNow = new Date();
+//        String strNow = sdf.format(dateNow);
+//        try {
+//            user1.setCreated(sdf.parse(strNow));
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        Set<Roles> strRoles = user.getListRoles();
+//        Set<Roles> listRoles = new HashSet<>();
+//        if (strRoles == null) {
+//            // Lấy vai trò người dùng mặc định
+//            Roles userRole = rolesService.findByRoleName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException("Default user role not found"));
+//            listRoles.add(userRole);
+//        }
+//
+//        else {
+//            strRoles.forEach(role->{
+//                if (role.equals("admin")) {
+//                    Roles admin = rolesService.findByRoleName(ERole.ROLE_ADMIN)
+//                            .orElseThrow(() -> new RuntimeException("Error is not found "));
+//                    listRoles.add(admin);
+//                } else if (role.equals("moderator")) {
+//                    Roles modRole = rolesService.findByRoleName(ERole.ROLE_MODERATOR)
+//                            .orElseThrow(() -> new RuntimeException("Error is not found"));
+//                    listRoles.add(modRole);
+//                } else if (role.equals("user")) {
+//                    Roles userRole = rolesService.findByRoleName(ERole.ROLE_USER)
+//                            .orElseThrow(() -> new RuntimeException("Error is not found"));
+//                    listRoles.add(userRole);
+//                }
+//            });
+//
+//        }
+//        user.setListRoles(listRoles);
+//        userService.saveOrUpdate(user);
+//
+//        return "redirect:/api/v1/auth/signin";
+//    }
+
+
     @PostMapping("/signup")
-    public String registerUser(@RequestBody User user) {
-//         if (userService.existsByUserName(signupRequest.getUserName())) {
-//             return ResponseEntity.badRequest().body(new MessageReponse("Error:Username is already"));
-//         }
-//         if (userService.existsByEmail(signupRequest.getEmail())) {
-//             return ResponseEntity.badRequest().body(new MessageReponse("Error:Mail is already"));
-//         }
+    public String registerUser(@ModelAttribute User user) { // Use @ModelAttribute instead of @RequestBody
 
-            if (userService.existsByUserName(user.getUserName())) {
-        // Return a message or redirect with an error message
-        return "redirect:/signup?error=username";
-    }
+        if (userService.existsByUserName(user.getUserName())) {
+            // Redirect with an error message
+            return "redirect:/signup?error=username";
+        }
 
-    if (userService.existsByEmail(user.getEmail())) {
-        // Return a message or redirect with an error message
-        return "redirect:/signup?error=email";
-    }
+        if (userService.existsByEmail(user.getEmail())) {
+            // Redirect with an error message
+            return "redirect:/signup?error=email";
+        }
+
         User user1 = new User();
         user1.setUserName(user.getUserName());
         user1.setPassword(encoder.encode(user.getPassword()));
@@ -75,7 +141,7 @@ public class UserController {
         user1.setPhone(user.getPhone());
         user1.setUserStatus(true);
 
-        //created ngày
+        // Created ngày
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date dateNow = new Date();
         String strNow = sdf.format(dateNow);
@@ -91,41 +157,39 @@ public class UserController {
             // Lấy vai trò người dùng mặc định
             Roles userRole = rolesService.findByRoleName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException("Default user role not found"));
             listRoles.add(userRole);
-        }
-
-        else {
-            strRoles.forEach(role->{
-                if (role.equals("admin")) {
+        } else {
+            strRoles.forEach(role -> {
+                if (role.getRoleName().equals("admin")) { // Use getRoleName() to compare role names
                     Roles admin = rolesService.findByRoleName(ERole.ROLE_ADMIN)
-                            .orElseThrow(() -> new RuntimeException("Error is not found "));
+                            .orElseThrow(() -> new RuntimeException("Error is not found"));
                     listRoles.add(admin);
-                } else if (role.equals("moderator")) {
+                } else if (role.getRoleName().equals("moderator")) {
                     Roles modRole = rolesService.findByRoleName(ERole.ROLE_MODERATOR)
                             .orElseThrow(() -> new RuntimeException("Error is not found"));
                     listRoles.add(modRole);
-                } else if (role.equals("user")) {
+                } else if (role.getRoleName().equals("user")) {
                     Roles userRole = rolesService.findByRoleName(ERole.ROLE_USER)
                             .orElseThrow(() -> new RuntimeException("Error is not found"));
                     listRoles.add(userRole);
                 }
             });
-
         }
+
         user.setListRoles(listRoles);
-        userService.saveOrUpdate(user);
+        userService.saveOrUpdate(user1); // Save user1 instead of user
 
         return "redirect:/api/v1/auth/signin";
     }
 
-@GetMapping("/signin")
-public String showSigninPage(){
+    @GetMapping("/signin")
+   public String showSigninPage(){
     return "login";
 }
 
     @PostMapping("/signin")
-    public String loginUser(@RequestBody LoginRequest loginRequest){
+    public String loginUser(@ModelAttribute  User user){
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword())
+                new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword())
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
@@ -138,4 +202,6 @@ public String showSigninPage(){
         return "home";
 
     }
+
+
 }
